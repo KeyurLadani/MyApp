8bee571b64884c38a4f6d1e63301c4138d5d1ea7
KeyurLadani <keyurl@softwareco.com>
2023-02-13 13:48:41 +0530

    myApp First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Home from './Component/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My App</h1>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
